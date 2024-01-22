9bbeee3b97c83cd15c77237febf32f1ee087ee2d
vahid <vahid.saber78@gmail.com>
2024-01-22 11:38:29 +0100

    Update build64.bat

--- src/odbc/rsodbc/rsodbc_setup/build64.bat
+++ src/odbc/rsodbc/rsodbc_setup/build64.bat
@@ -5,7 +5,8 @@ echo Building 64 bit Windows Redshift ODBC Setup DLL
 echo Setting environment variables
 dir "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
 
-devenv /Rebuild "Release|x64" rsodbcsetup.sln
+#devenv /Rebuild "Release|x64" rsodbcsetup.sln
+msbuild rsodbcsetup.sln /t:Rebuild /p:Configuration=Release /p:Platform=x64  /verbosity:detailed
 
 echo Done building 64 bit Windows Redshift ODBC Setup DLL
 
